b2f66aae56533d9ef80c9e1a4ecd9e4bc82fa3e8
Lorenzo <lorenzopica31@gmail.com>
2025-06-09 16:53:15 +0200

    created basic react boilerplate

---
+++ CV-Application/src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <div id='form'>
+      <h1>Name</h1>
+      
+
+
+
+    </div>
+   
+
+
+
+    </> 
+  )
+}
+
+export default App
