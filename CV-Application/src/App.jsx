e20daf94397f513b9ab97c704ee91f494a17f2cf
Lorenzo <lorenzopica31@gmail.com>
2025-06-14 16:20:10 +0200

    project complete

--- CV-Application/src/App.jsx
+++ CV-Application/src/App.jsx
@@ -1,68 +1,89 @@
 import { useState } from 'react'
-/*import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'*/
 import './App.css'
 
 function App() {
-  const [form, setForm] = useState(0)
+  const [isSubmitted, setSubmission] = useState(false)
+  const [formData, setFormData] = useState({
+    name: '',
+    surname: '',
+    email: '',
+    phone: '',
+    address: '',
+    workExperience: '',
+    education: ''
+  })
+
+  const handleChange = (e) => {
+    const { name, value } = e.target
+    setFormData(prevData => ({
+      ...prevData,
+      [name]: value
+    }))
+  }
+
+  const handleSubmit = () => {
+    setSubmission(true)
+  }
 
   return (
     <>
     <div id="form-container">
-    <div className='form'>
-      <h1>Name</h1>
-      <input type='text' placeholder='Enter your name' />
+       
+       <div className='form'>
+          <h1>Name</h1>
+          <input type='text' name='name' placeholder='Enter your name' onChange={handleChange} />
 
-        <h1>Surname</h1>
-        <input type='text' placeholder='Enter your surname' />
+          <h1>Surname</h1>
+          <input type='text' name='surname' placeholder='Enter your surname' onChange={handleChange} />
 
-        <h1>Email</h1>
-        <input type='email' placeholder='Enter your email' />
+          <h1>Email</h1>
+          <input type='email' name='email' placeholder='Enter your email' onChange={handleChange} />
 
-        <h1>Phone Number</h1>
-        <input type='tel' placeholder='Enter your phone number' />
+          <h1>Phone Number</h1>
+          <input type='tel' name='phone' placeholder='Enter your phone number' onChange={handleChange} />
 
-        <h1>Address</h1>
-        <input type='text' placeholder='Enter your address' />
+          <h1>Address</h1>
+          <input type='text' name='address' placeholder='Enter your address' onChange={handleChange} />
 
-        <h1>Work Experience</h1>
-        <input type='text' placeholder='Enter your work experience' />
+          <h1>Work Experience</h1>
+          <input type='text' name='workExperience' placeholder='Enter your work experience' onChange={handleChange} />
 
-        <h1>Education</h1>
-        <input type='text' placeholder='Enter your education' />
+          <h1>Education</h1>
+          <input type='text' name='education' placeholder='Enter your education' onChange={handleChange} />
 
-        <br />
-        <button type='submit'>Submit</button>
-       </div>
-   
-       <div className='form' id='second-form'>
-      <h1>Name</h1>
-      <input type='text'  readOnly/>
+          <br />
+          <button className='submit' type='submit' onClick={handleSubmit}>Submit</button>
+        </div>
 
-        <h1>Surname</h1>
-        <input type='text'  readOnly />
+        
+        <div className='form' id='second-form'>
+          <h1>Name</h1>
+          <input type='text' readOnly value={isSubmitted ? formData.name : ''} />
 
-        <h1>Email</h1>
-        <input type='email'  readOnly/>
+          <h1>Surname</h1>
+          <input type='text' readOnly value={isSubmitted ? formData.surname : ''} />
 
-        <h1>Phone Number</h1>
-        <input type='tel'  readOnly/>
+          <h1>Email</h1>
+          <input type='email' readOnly value={isSubmitted ? formData.email : ''} />
 
-        <h1>Address</h1>
-        <input type='text'  readOnly/>
+          <h1>Phone Number</h1>
+          <input type='tel' readOnly value={isSubmitted ? formData.phone : ''} />
 
-        <h1>Work Experience</h1>
-        <input type='text'  readOnly/>
+          <h1>Address</h1>
+          <input type='text' readOnly value={isSubmitted ? formData.address : ''} />
 
-        <h1>Education</h1>
-        <input type='text' readOnly/>
+          <h1>Work Experience</h1>
+          <input type='text' readOnly value={isSubmitted ? formData.workExperience : ''} />
 
-        <br />
+          <h1>Education</h1>
+          <input type='text' readOnly value={isSubmitted ? formData.education : ''} />
+
+          <br />
+        </div>
       </div>
-      </div>
-
        </> 
   )
+   
 }
 
 export default App
